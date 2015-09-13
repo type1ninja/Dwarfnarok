e4100081f5d9abd83be2e600894f5690580b87e9
Type1Ninja <type1ninja@gmail.com>
2015-09-13 18:16:04 -0500

    Added Movable Player, Unused "Weapon" class
    Player prefab with camera. Uses Unity Documentation move script and
    Simple Smooth Mouse Look. Added Unused "Weapon" class, with various
    variables representing weapon stats. Also added a "WeaponType" enum,
    with types SWORD, AXE, HAMMER, and SHIELD.

---
+++ Assets/Scripts/PlayerEquipment/Weapon.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+	public float handleLength = 1.0f;
+	public float headSize = 1.0f;
+	public float dmgMod = 1.0f;
+	public float knockbackMod = 1.0f;
+	//Armor mods are for shields
+	public float passiveArmorMod = 0.0f;
+	public float blockingArmorMod = 0.0f;
+	public float maxAttackTime = 1.0f;
+	//Default to sword, but this should be set EVERY TIME
+	public WeaponType type = WeaponType.SWORD;
+
+	float currentAttackTime;
+	bool isAttacking = false;
+
+	void Start() {
+		currentAttackTime = maxAttackTime;
+	}
+
+	void FixedUpdate() {
+		if (isAttacking) {
+			currentAttackTime -= Time.fixedDeltaTime;
+			if (currentAttackTime <= 0) {
+				isAttacking = false;
+				currentAttackTime = maxAttackTime;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerEquipment/WeaponType.cs
@@ -0,0 +1,7 @@
+﻿public enum WeaponType {
+	SWORD, AXE, HAMMER, SHIELD
+	//'SWORD' identifies swords, spears, and daggers
+	//'AXE' identifies... axes
+	//'HAMMER' identifies hammers and punching gloves
+	//'SHIELD' identifies shields
+}
