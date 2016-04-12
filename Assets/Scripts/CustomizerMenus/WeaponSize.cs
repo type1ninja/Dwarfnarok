98f322247872c7f50330567e3f80279d1d2926c1
Type1Ninja <type1ninja@gmail.com>
2016-04-12 17:13:26 -0500

    Tweaked Weapon Scaling
    Placement of the "blades" of the three different weapons now places the
    blade on the handle tip (and then offsets it back "into" the handle for
    hammers/axes). It does this by adding half of the handle scale to the
    placement, then subtracting half of the blade length, then offsetting
    it. Also tweaked the swords' default scaling/placement.

--- Assets/Scripts/CustomizerMenus/WeaponSize.cs
+++ Assets/Scripts/CustomizerMenus/WeaponSize.cs
@@ -4,36 +4,39 @@ using System.Collections;
 public class WeaponSize : MonoBehaviour {
 
 	//Sword scales
-	static Vector3 DEFAULT_SWORD_BLADE_SCALE = new Vector3 (0.3f, 1.5f, 0.1f);
+	static Vector3 DEFAULT_SWORD_BLADE_SCALE = new Vector3 (0.45f, 2f, 0.1f);
 	static Vector3 DEFAULT_SWORD_HANDLE_SCALE = new Vector3 (0.15f, 0.4f, 0.1f);
-	static Vector3 DEFAULT_SWORD_HANDLE_POS = new Vector3 (0, 0.476f, 0.757f);
-	static float DEFAULT_SWORD_BLADE_OFFSET = 0.546f;
+	static Vector3 DEFAULT_SWORD_HANDLE_POS = new Vector3 (0, 0, 0.757f);
+	static float DEFAULT_SWORD_BLADE_OFFSET = 0f;
+	static float DEFAULT_SWORD_HANDLE_LENGTH_OFFSET = -1.25f;
 
 	//Axe scales
 	static Vector3 DEFAULT_AXE_BLADE_SCALE = new Vector3 (0.8f, 0.8f, 0.1f);
 	static Vector3 DEFAULT_AXE_HANDLE_SCALE = new Vector3 (0.15f, 1.6f, 0.15f);
 	static Vector3 DEFAULT_AXE_HANDLE_POS = new Vector3 (0, 1.024f, 0.757f);
-	static float DEFAULT_AXE_BLADE_OFFSET = -1.225f;
+	static float DEFAULT_AXE_BLADE_OFFSET = -0.1f;
 
 	//Hammer scales
 	static Vector3 DEFAULT_HAMMER_BLADE_SCALE = new Vector3 (0.6f, 0.5f, 0.5f);
 	static Vector3 DEFAULT_HAMMER_HANDLE_SCALE = new Vector3 (0.15f, 1.6f, 0.15f);
 	static Vector3 DEFAULT_HAMMER_HANDLE_POS = new Vector3 (0, 1.024f, 0.757f);
-	static float DEFAULT_HAMMER_BLADE_OFFSET = -1.097f;
+	static float DEFAULT_HAMMER_BLADE_OFFSET = -0.1f;
 
 	public Transform bladeTrans;
 	public Transform handleTrans;
 
+	//Note that there ARE some differences between sword, axe, and hammer calculations
 	public void UpdateSize(WeaponType newType, float bladeScale, float handleScale) {
 		//TODO - make small blade + large handle not horribly broken
 		if (newType == WeaponType.SWORD) {
 
-			handleTrans.localScale = new Vector3 (DEFAULT_SWORD_HANDLE_SCALE.x, DEFAULT_SWORD_HANDLE_SCALE.y * handleScale, DEFAULT_SWORD_HANDLE_SCALE.z);
+			//The sword handle scale gets a special calculation applied to map the 1-2 scale to a 1-12 scale
+			handleTrans.localScale = new Vector3 (DEFAULT_SWORD_HANDLE_SCALE.x, (DEFAULT_SWORD_HANDLE_SCALE.y * (((handleScale - 1.6f) * 6f) + 10f)) + DEFAULT_SWORD_HANDLE_LENGTH_OFFSET, DEFAULT_SWORD_HANDLE_SCALE.z);
 			handleTrans.localPosition = DEFAULT_SWORD_HANDLE_POS;
 			
 			bladeTrans.localScale = new Vector3 (DEFAULT_SWORD_BLADE_SCALE.x * bladeScale, DEFAULT_SWORD_BLADE_SCALE.y * bladeScale, DEFAULT_SWORD_BLADE_SCALE.z);
 			//Weird math to get the blade in the right spot
-			bladeTrans.localPosition = new Vector3 (DEFAULT_SWORD_HANDLE_POS.x, handleTrans.localPosition.y + (DEFAULT_SWORD_HANDLE_SCALE.y * handleScale) + (DEFAULT_SWORD_BLADE_OFFSET * handleScale), DEFAULT_SWORD_HANDLE_POS.z);
+			bladeTrans.localPosition = new Vector3 (DEFAULT_SWORD_HANDLE_POS.x, handleTrans.localPosition.y + (handleTrans.localScale.y / 2) + (bladeTrans.localScale.y / 2) + DEFAULT_SWORD_BLADE_OFFSET, DEFAULT_SWORD_HANDLE_POS.z);
 		
 		} else if (newType == WeaponType.AXE) {
 
@@ -42,9 +45,8 @@ public class WeaponSize : MonoBehaviour {
 
 			bladeTrans.localScale = new Vector3 (DEFAULT_AXE_BLADE_SCALE.x * bladeScale, DEFAULT_AXE_BLADE_SCALE.y * bladeScale, DEFAULT_AXE_BLADE_SCALE.z);
 			//Weird math to get the blade in the right spot
-			bladeTrans.localPosition = new Vector3 (DEFAULT_AXE_HANDLE_POS.x, handleTrans.localPosition.y + (DEFAULT_AXE_HANDLE_SCALE.y * handleScale) + (DEFAULT_AXE_BLADE_OFFSET * handleScale), DEFAULT_AXE_HANDLE_POS.z);
+			bladeTrans.localPosition = new Vector3 (DEFAULT_AXE_HANDLE_POS.x, handleTrans.localPosition.y + (handleTrans.localScale.y / 2) - (bladeTrans.localScale.y / 2) + DEFAULT_AXE_BLADE_OFFSET, DEFAULT_AXE_HANDLE_POS.z);
 
-		
 		} else if (newType == WeaponType.HAMMER) {
 
 			handleTrans.localScale = new Vector3 (DEFAULT_HAMMER_HANDLE_SCALE.x, DEFAULT_HAMMER_HANDLE_SCALE.y * handleScale, DEFAULT_HAMMER_HANDLE_SCALE.z);
@@ -52,8 +54,7 @@ public class WeaponSize : MonoBehaviour {
 			
 			bladeTrans.localScale = new Vector3 (DEFAULT_HAMMER_BLADE_SCALE.x * bladeScale, DEFAULT_HAMMER_BLADE_SCALE.y * bladeScale, DEFAULT_HAMMER_BLADE_SCALE.z * bladeScale);
 			//Weird math to get the blade in the right spot
-			bladeTrans.localPosition = new Vector3 (DEFAULT_HAMMER_HANDLE_POS.x, handleTrans.localPosition.y + (DEFAULT_HAMMER_HANDLE_SCALE.y * handleScale) + (DEFAULT_HAMMER_BLADE_OFFSET * handleScale), DEFAULT_HAMMER_HANDLE_POS.z);
-
+			bladeTrans.localPosition = new Vector3 (DEFAULT_HAMMER_HANDLE_POS.x, handleTrans.localPosition.y + (handleTrans.localScale.y / 2) - (bladeTrans.localScale.y / 2) + DEFAULT_HAMMER_BLADE_OFFSET, DEFAULT_HAMMER_HANDLE_POS.z);
 		}
 	}
 }
